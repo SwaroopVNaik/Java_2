e5be75731a2c64ae2e2f160d6c2232b8ef6ac211
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-07-22 15:30:15 +0530

    day 2

---
+++ Loops.java
@@ -0,0 +1,25 @@
+public class Loops {
+    public static void main(String[] args) {
+
+        // For Loop 
+        // counter ++ => counter = counter + 1 
+        // for(int counter = 0 ; counter < 11 ; counter ++ ){
+        //     System.out.print(counter + " ");
+        // }
+
+        // While Loop
+        // int counter = 0;
+        // while(counter < 11){
+        //     System.out.print(counter + " ");
+        //     counter = counter + 1;
+        // }
+
+        // Do - While Loop 
+        int counter = 0; 
+        do{
+            System.out.println(counter);
+            counter = counter + 1;
+        }while(counter < 11);
+
+    }
+}
---
+++ LoopsPrac.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class LoopsPrac {
+    public static void main(String[] args) {
+        
+        // Q) Print the sum of First n Natural Numbers
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter any natural number : ");
+        int number = sc.nextInt();
+
+        // int sum = 0 ;
+        // for(int counter = 0 ; counter <= number ; counter = counter + 1){
+        //     sum = sum + counter;
+        // }
+
+        // System.out.println(sum);
+
+        // Q)Print the table of a number input by the user
+
+        // for(int counter = 1 ; counter < 11 ; counter = counter + 1){
+
+        //     System.out.println(number + "x" + counter + "=" + (number * counter));
+
+        // }
+
+        // Q) Print all even numbers till n 
+
+        // System.out.println("The even Numbers in " + number + ":");
+        // for(int counter = 0 ; counter <= number ; counter = counter + 1){
+        //     if (counter % 2 == 0){
+        //         System.out.println(counter);
+        //     }
+        // }
+
+    }
+}
